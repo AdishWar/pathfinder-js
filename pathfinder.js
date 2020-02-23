021cd43a41ae810ca93a2b19df8b9a8c19d41dcb
adish <adishwarjain767@gmail.com>
2020-02-23 16:03:43 +0530

    added drawing based on pathfinder logic, some errors remain...

--- pathfinder.js
+++ pathfinder.js
@@ -1,7 +1,7 @@
 
-
+canvasSize = 600;
 function setup() {
-    createCanvas(650,650);
+    createCanvas(canvasSize,canvasSize);
 
     pixelDensity(1);
     noStroke();
@@ -10,9 +10,10 @@ function setup() {
 }
 
 // GLOBAL PARAMETERS
-arrSize = 10;
+arrSize = 50;
 startCoord = [1,1];
-endCoord = [8,9];
+endCoord = [40,32];
+cellSize = canvasSize/arrSize;
 
 // END 
 
@@ -34,6 +35,10 @@ for(i = 0; i < arrSize; i++) {
     arr.push(a)
 }
 
+function cell (x, y, size) {
+    rect(x,y,size,size);
+};
+
 function heurstic(currCoord, endCoord) {
     distance = Math.abs(currCoord[0] - endCoord[0]) + Math.abs( currCoord[1] - endCoord[1] );
     return(distance);
@@ -46,15 +51,43 @@ function dist2here(startCoord, currCoord) {
 
 function contained_in(coord, arr) {
     // returns false if coord is not in arr
-    len = arr.length;
+    // returns true if coord is in arr
+
     flag = false;
 
-    for(i = 0; i < len; i++) {
+    for(i = 0; i < arr.length; i++) {
         if(arr[i][0] == coord[0] && arr[i][1] == coord[1] ) {
             flag = true;
         }
     }
+    console.log("flag", flag);
     return(flag);
+}
+
+function clean_array(nayborArr, blockedArr) {
+    cleanArr = [];
+
+    for(i = 0; i < nayborArr.length; i++) {
+        x = nayborArr[i][0];
+        y = nayborArr[i][1];
+        
+        isClean = true;
+        
+        for(j = 0; j < blockedArr.length; j++) {
+            blockedX = blockedArr[j][0];
+            blockedY = blockedArr[j][1];
+
+            if(x == blockedX && y == blockedY ) {
+                isClean = false;
+            }
+        }
+
+        if (isClean) {
+            cleanArr.push([x,y]);
+        }
+    }
+
+    return(cleanArr);
 }
 
 function neighbourCoords(coord) {
@@ -65,23 +98,21 @@ function neighbourCoords(coord) {
 
     for(i = -1; i < 2; i++) {
         for(j = -1; j < 2; j++) {
-            neighbourX = x + i;
-            neighbourY = y + j;
-            cell = [neighbourX, neighbourY];
-
-            if(i == 0 && j==0) {
-                continue;
-            }
-            else if ( !contained_in(cell, blockedPoints) ) {
-                neighbours.push(cell);
+            neighbourCell = [ x+i, y+j ];
+            
+            if((i == 0 && j==0)) {
+                // do nothin
             }
             else {
-                
+                neighbours.push(neighbourCell);
             }
+            
         }
-        
     }
-    return(neighbours);
+
+    clean_neighbours = clean_array(neighbours, blockedPoints);
+    
+    return(clean_neighbours);
 }
 
 function bestNeigbour(currCoord, endCoord) {
@@ -102,26 +133,74 @@ function bestNeigbour(currCoord, endCoord) {
     return(bestNaybor);
 }
 
-function  path(startCoord, endCoord) {
+function path(startCoord, endCoord) {
     pathCoords = []
 
     currCoord = startCoord;
 
     while( heurstic(currCoord, endCoord) != 0 ) {
-
         nextHop = bestNeigbour(currCoord, endCoord);
 
         pathCoords.push(nextHop);
-        console.log(nextHop);
         currCoord = nextHop;
     }
 
     return(pathCoords);
 }
 
-console.log( path( [2,2], [8,8] ) );
+function drawCells() {
+
+    blockedPts = [
+        [4,6],
+        [5,6],
+        [6,6],
+        [7,6]
+    ];
+
+    pathh = path(startCoord, endCoord);
+    // console.log(pathh);
+
+    for( i = 0; i < arrSize; i++) {
+        for(j = 0; j < arrSize; j++) {
+            thisCell = [i,j];
+            
+            isContainedInBlocked = contained_in(thisCell, blockedPts);
+            console.log(isContainedInBlocked);
+            // isContainedInPath = contained_in(thisCell, pathh);
+            
+            if(thisCell[0] == startCoord[0] && thisCell[1] == startCoord[1]) {
+                // thisCell is startcell
+                fill('red');
+            } else if( thisCell[0] == endCoord[0] && thisCell[1] == endCoord[1] ) {
+                // thisCell is endCell
+                fill('green');
+            }
+            // else if ( isContainedInBlocked == true ) {
+            //     // thisCell is blockedCell
+            //     fill('blue');
+            // }
+
+            // else if (isContainedInPath == true) {
+            //     // thisCell is in path
+            //     // fill('white');
+            // }
+
+            else {
+                fill(200);
+            }
+
+
+            cell(i*cellSize,j*cellSize,cellSize,cellSize);
+        }
+    }
+
+}
 
 function draw() {
 
+    drawCells();
 
 }
+
+// isContainedInPath = contained_in([2,3], [ [2,4],[4,4] ] );
+// console.log(isContainedInPath);
